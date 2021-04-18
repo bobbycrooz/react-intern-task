31dcd756f982d4570c4ad271df05190aecd9e552
bobbycrooz <idrisloove@gmail.com>
2021-04-18 09:32:07 -0700

    almost done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Header,SelectCurrency, RecentActivities, Footer } from "./components";
+import { Header, RecentActivities, Footer } from "./components";
 
 
 
